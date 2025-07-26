1911cf798428eb61934a4568acfdc2c8c27d1c6d
Ding-Han Sean Yan <sean@dhsy.me>
2025-07-26 13:31:52 -0700

    refactor: lazy.lua bootstrap improvement, and option passed in lazy improvement

--- init.lua
+++ init.lua
@@ -1,27 +1,37 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
 -- [[ Lazy.nvim Bootstrap Install]]
 -- Taken from lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
+----------------------------------------------------------------------------
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
 
 local opts = {
-    lockfile = "/home/tzen/.dotfiles/nvim/lazy-lock.json", -- lockfile generated after running update.
+    lockfile = "/home/tzen/.dotfiles/nvim.config/lazy-lock.json", -- lockfile generated after running update.
+    rocks = {
+        enabled = false,
+        hererocks = false,
+    },
+    spec = { import = "plugins" } ,
+
 }
 -- "plugins" is requiring the lua module in /nvim/lua/plugins.lua
-require("lazy").setup("plugins", opts);
+require("lazy").setup(opts);
 
 -- [[ Keymaps ]]
 require("keymaps");
