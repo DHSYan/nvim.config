92844891bf88c1cbfcedb166fb65e6343a0d8083
Ding-Han Sean Yan <sean@dhsy.me>
2025-07-26 13:37:40 -0700

    chore: cleanup keymaps file

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,9 +1,3 @@
--- vim.keymap.set("n", " ", "<nop>")
--- vim.g.mapleader = " "
-
--- vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
-
-
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 
@@ -13,11 +7,6 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 -- This allows pasting but doesn't pop it out of the register
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
--- vim.keymap.set("n", "Q", "<nop>") -- <nop> means nothing
--- vim.keymap.set("n", "q", "<nop>")
--- vim.keymap.set("v", "Q", "<nop>")
--- vim.keymap.set("v", "q", "<nop>")
-
 vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
 
@@ -25,10 +14,7 @@ vim.keymap.set("n", "<leader>nh", vim.cmd.noh)
 
 vim.keymap.set('n', '<leader>t', ":!")
 
--- vim.keymap.set('n', '<leader>g', ":Git")
 vim.keymap.set('n', '<leader>G', vim.cmd.LazyGit)
-
--- vim.keymap.set('n', '<leader>md', ":!md-to-pdf *.md && open *.pdf<CR>")
 
 vim.keymap.set('n', '<leader>i', vim.cmd.PasteImg)
 
@@ -40,12 +26,7 @@ vim.keymap.set("n", "<leader>k", "<C-w>k")
 
 -- System CLipboard Interaction
 vim.keymap.set("n", "<leader>sp", '"+p')
--- vim.keymap.set("v", "<leader>sp", '"+p')
---
--- vim.keymap.set("n", "<leader>sy", '"+y')
+vim.keymap.set("v", "<leader>sp", '"+p')
+vim.keymap.set("n", "<leader>sy", '"+y')
 vim.keymap.set("v", "<leader>sy", '"+y')
-
--- vim.keymap.set("n", "<leader>ss", ":w<CR>:silent !/Applications/Skim.app/Contents/SharedSupport/displayline <C-r>=line('.')<CR> %<.pdf<CR>:!focusvim<CR><CR>")
--- vim.keymap.set("n", "<leader>v", ":VimtexView<CR>")
--- vim.keymap.set("n", "<leader>ob", ":Lazy load obsidian.nvim<CR>")
 
