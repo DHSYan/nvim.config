cad165bf131b4f2f86af0a349b939d1a316ceb17
DH Sean Yan <sean@dhsy.dev>
2025-11-20 18:11:59 -0800

    feat(plugins): telescope

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,7 +2,7 @@ vim.pack.add({
     { src = 'https://github.com/neovim/nvim-lspconfig' },
     { src = 'https://github.com/stevearc/oil.nvim' },
     { src = 'https://github.com/nvim-tree/nvim-web-devicons' },
-    { src = 'https://github.com/echasnovski/mini.pick' },
+    { src = 'https://github.com/nvim-telescope/telescope.nvim' },
     { src = 'https://github.com/shortcuts/no-neck-pain.nvim' },
     { src = 'https://github.com/L3MON4D3/LuaSnip' },
     { src = "https://github.com/nvim-lua/plenary.nvim" },
@@ -19,7 +19,8 @@ nmap("<leader>e", "<CMD>Oil<CR>", "Open Parent Directory")
 
 reload = require("plenary.reload")
 require("plugins-options.oil")
-require("plugins-options.mini")
+-- require("plugins-options.mini")
+require("plugins-options.telescope")
 require("snippets.mysnips")
 require("plugins-options.obsidian")
 
