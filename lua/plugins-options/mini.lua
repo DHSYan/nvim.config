296382d162392018b957bb4b7bb62f11a5ecad91
DH Sean Yan <sean@dhsy.dev>
2025-12-05 12:31:09 -0800

    WIP: remove current conifg, prep for LazyVim

--- lua/plugins-options/mini.lua
+++
@@ -1,5 +0,0 @@
-require "mini.pick".setup()
-
-vim.keymap.set('n', '<leader>f', ':Pick files<CR>')
-vim.keymap.set('n', '<leader>h', ':Pick help<CR>')
-
--- lua/util/nmap.lua
+++
@@ -1,8 +0,0 @@
-local function nmap(key, fn, meaning)
-    vim.keymap.set('n', key, fn, {desc = meaning})
-end
-
-
-return nmap -- I think this is ok?
-
-
